30997acbda741c0251a4a31fe14caf2de58ad38a
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2022-04-13 08:30:11 -0300

    feat: first steps and implementing widgets

--- projeto-introducao/bytebank/lib/main.dart
+++ projeto-introducao/bytebank/lib/main.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text('Deliver features faster', textDirection: TextDirection.ltr),
-          Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-          Column(
-            children: const <Widget>[
-              Text('Craft beautiful UIs', textDirection: TextDirection.ltr),
-            ],
-          )
-        ],
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Icon(Icons.add),
+        ),
+        body: ListaTransferencia(),
+      ),
+    ));
+
+class ListaTransferencia extends StatelessWidget {
+  const ListaTransferencia({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(150.0, 215488)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  const ItemTransferencia(this._transferencia, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
       ),
     );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
